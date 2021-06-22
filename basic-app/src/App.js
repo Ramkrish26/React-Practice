45f673ea415eab4acb57f9c0cc1a9ff4edd28608
Ramakrishnan Rajaram <ramakrishnan2629@gmail.com>
2021-06-22 22:39:08 +0530

    React Day 2 (#1)

--- basic-app/src/App.js
+++ basic-app/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 import Greet from './components/Greet';
+import Welcome from './components/Welcome';
+import Message from './components/Message';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
-      <Greet/>
+      {/* <Greet name="Ram krish">
+        <p>I am a automation tester</p>
+      </Greet>
+      <Greet name="Harry"/>
+      <Greet name="Ron"/>
+      <Welcome name="Ram krish"/>
+      <Welcome name="Ron"/>
+      <Welcome name="Harry"/> 
+      <Message/>*/}
+      <Counter/>
     </div>
   );
 }
